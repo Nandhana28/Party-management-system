98b74cf40c2ca6e2baa8f6a09211339155d9ea60
Nandhana S <148519259+Nandhana28@users.noreply.github.com>
2025-10-02 14:30:18 +0530

    Create adminLogin.sql

---
+++ Backend/SQL files/adminLogin.sql
@@ -0,0 +1,11 @@
+USE PROJECT;
+
+DROP TABLE IF EXISTS login1;
+
+CREATE TABLE login1 (
+    username VARCHAR(50),
+    pass VARCHAR(50)
+);
+
+INSERT INTO login1 VALUES ('nandy', 'nan123');
+INSERT INTO login1 VALUES ('ritubg', 'ritubg');
