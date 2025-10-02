2168244c74be3fe1459cf491fd82425daed2102b
Nandhana S <148519259+Nandhana28@users.noreply.github.com>
2025-10-02 14:19:12 +0530

    Create HallSelection.js

---
+++ Frontend/src/components/HallSelection.js
@@ -0,0 +1,10 @@
+function HallSelection() {
+    // Hall selection logic here
+    return (
+      <div>
+        <h2>Select a Hall</h2>
+        {/* Hall selection UI */}
+      </div>
+    );
+  }
+  export default HallSelection;
